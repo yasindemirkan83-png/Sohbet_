e6fc8e8996c18e300c4a3f58fe61581dfd248ec2
yasindemirkan83-png <yasindemirkan83@gmail.com>
2026-01-31 12:41:59 +0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,7 +1,7 @@
-// firebaseConfig.js
+// Firebase Config ve Initialization
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getDatabase, ref, set, push, onValue, remove, update } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDE5-fq9ifhtBlXTVOR1PhzD3XuVOWSVeE",
@@ -13,8 +13,28 @@ const firebaseConfig = {
   appId: "1:210449601127:web:b3339666a09822b40c7ac2"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
-export const database = getDatabase(app);
+export const db = getDatabase(app);
+
+/* LOGIN */
+const googleBtn = document.getElementById("googleLogin");
+googleBtn.onclick = () => {
+  signInWithPopup(auth, provider);
+};
+
+onAuthStateChanged(auth, user => {
+  if(user){
+    document.getElementById("login").style.display="none";
+    document.getElementById("app").style.display="block";
+    loadTasks();
+    loadMessages();
+    loadUsers();
+  }
+});
+
+export function logout(){
+  signOut(auth);
+  location.reload();
+}

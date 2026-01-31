10690e8b7fd4941ba4a674262ead29c17326de57
yasindemirkan83-png <yasindemirkan83@gmail.com>
2026-01-31 13:28:26 +0300

    Update Service-Worker.js

--- Service-Worker.js
+++ Service-Worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "anfa-app-cache-v1";
+const CACHE_NAME = "anfa-pwa-cache-v1";
 const urlsToCache = [
   "./",
   "./index.html",
@@ -7,38 +7,30 @@ const urlsToCache = [
   "./icon-192.png",
   "./icon-512.png",
   "./uygulamaici.mp4",
-  "./açılış .mp4",
   "./google-symbol.png"
 ];
 
-self.addEventListener("install", (event) => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log("Cache açıldı ve dosyalar eklendi");
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
+    caches.match(event.request).then(response=>{
       return response || fetch(event.request);
     })
   );
 });
 
-self.addEventListener("activate", (event) => {
-  const cacheWhitelist = [CACHE_NAME];
+self.addEventListener("activate", event=>{
+  const cacheWhitelist=[CACHE_NAME];
   event.waitUntil(
-    caches.keys().then((keyList) =>
-      Promise.all(
-        keyList.map((key) => {
-          if (!cacheWhitelist.includes(key)) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
+    caches.keys().then(keys=>Promise.all(
+      keys.map(key=>{
+        if(!cacheWhitelist.includes(key)) return caches.delete(key);
+      })
+    ))
   );
 });

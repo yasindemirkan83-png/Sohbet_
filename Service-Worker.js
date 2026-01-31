eb48f6bd875bc1b3a6ad2bba14edf6bc52c2c793
yasindemirkan83-png <yasindemirkan83@gmail.com>
2026-01-31 14:03:04 +0300

    Update Service-Worker.js

--- Service-Worker.js
+++ Service-Worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'anfa-pwa-cache-v1';
-const urlsToCache = [
+const CACHE_NAME='anfa-pwa-cache-v1';
+const urlsToCache=[
   '/',
   '/index.html',
   '/icon-192.png',
@@ -12,31 +12,15 @@ const urlsToCache = [
   'https://www.gstatic.com/firebasejs/10.6.1/firebase-auth.js'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache=>{
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)));
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response=>{
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch',event=>{
+  event.respondWith(caches.match(event.request).then(resp=>resp || fetch(event.request)));
 });
 
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(keyList =>
-      Promise.all(keyList.map(key=>{
-        if(!cacheWhitelist.includes(key)){
-          return caches.delete(key);
-        }
-      }))
-    )
-  );
+self.addEventListener('activate',event=>{
+  const whitelist=[CACHE_NAME];
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>!whitelist.includes(k)?caches.delete(k):null))));
 });

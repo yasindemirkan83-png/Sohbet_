8e0d802e05f4d95da1ac653e995015d2bba825d2
yasindemirkan83-png <yasindemirkan83@gmail.com>
2026-01-31 10:30:01 +0300

    Add files via upload

---
+++ Service-Worker.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'sohbet-cache-v1';
+const urlsToCache = ['index.html','app.js','manifest.json'];
+
+self.addEventListener('install', event=>{
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache=>{
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event=>{
+  event.respondWith(
+    caches.match(event.request).then(response=>{
+      return response || fetch(event.request);
+    })
+  );
+});

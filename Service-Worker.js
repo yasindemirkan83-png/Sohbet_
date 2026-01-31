08bae7414667f3387f3775a6b5b23645dc55eb81
yasindemirkan83-png <yasindemirkan83@gmail.com>
2026-01-31 13:52:07 +0300

    Update Service-Worker.js

--- Service-Worker.js
+++ Service-Worker.js
@@ -1,22 +1,26 @@
-const CACHE_NAME = "anfa-pwa-cache-v1";
+const CACHE_NAME = 'anfa-pwa-cache-v1';
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./app.js",
-  "./firebaseConfig.js",
-  "./icon-192.png",
-  "./icon-512.png",
-  "./uygulamaici.mp4",
-  "./google-symbol.png"
+  '/',
+  '/index.html',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/uygulamaici.mp4',
+  '/açılış .mp4',
+  '/google-symbol.png',
+  'https://www.gstatic.com/firebasejs/10.6.1/firebase-app.js',
+  'https://www.gstatic.com/firebasejs/10.6.1/firebase-database.js',
+  'https://www.gstatic.com/firebasejs/10.6.1/firebase-auth.js'
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache=>{
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response=>{
       return response || fetch(event.request);
@@ -24,13 +28,15 @@ self.addEventListener("fetch", event => {
   );
 });
 
-self.addEventListener("activate", event=>{
-  const cacheWhitelist=[CACHE_NAME];
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
-    caches.keys().then(keys=>Promise.all(
-      keys.map(key=>{
-        if(!cacheWhitelist.includes(key)) return caches.delete(key);
-      })
-    ))
+    caches.keys().then(keyList =>
+      Promise.all(keyList.map(key=>{
+        if(!cacheWhitelist.includes(key)){
+          return caches.delete(key);
+        }
+      }))
+    )
   );
 });

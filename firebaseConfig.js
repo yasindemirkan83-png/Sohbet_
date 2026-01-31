069416031b9ebb2dc8a4bd4c36ac03a84a35aabf
yasindemirkan83-png <yasindemirkan83@gmail.com>
2026-01-31 14:01:12 +0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,9 +1,9 @@
-// firebaseConfig.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getDatabase, ref, push, onValue, remove, update } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+// Firebase Config ve Başlatma
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.1/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.6.1/firebase-database.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.6.1/firebase-auth.js";
 
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyDE5-fq9ifhtBlXTVOR1PhzD3XuVOWSVeE",
   authDomain: "studio-3972799448-609d1.firebaseapp.com",
   databaseURL: "https://studio-3972799448-609d1-default-rtdb.firebaseio.com",
@@ -13,8 +13,7 @@ export const firebaseConfig = {
   appId: "1:210449601127:web:b3339666a09822b40c7ac2"
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
-export const db = getDatabase(app);

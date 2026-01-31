b546d9678a10122e572c5e379ca0737e64ad2aed
yasindemirkan83-png <yasindemirkan83@gmail.com>
2026-01-31 11:00:05 +0300

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,15 @@
+// firebaseConfig.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyDE5-fq9ifhtBlXTVOR1PhzD3XuVOWSVeE",
+    authDomain: "studio-3972799448-609d1.firebaseapp.com",
+    databaseURL: "https://studio-3972799448-609d1-default-rtdb.firebaseio.com",
+    projectId: "studio-3972799448-609d1",
+    storageBucket: "studio-3972799448-609d1.firebasestorage.app",
+    messagingSenderId: "210449601127",
+    appId: "1:210449601127:web:b3339666a09822b40c7ac2"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);

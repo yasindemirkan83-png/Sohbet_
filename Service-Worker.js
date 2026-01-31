f4fa5172828ca9fe6cbf9e7383b338fff80c1984
yasindemirkan83-png <yasindemirkan83@gmail.com>
2026-01-31 11:31:32 +0300

    Update Service-Worker.js

--- Service-Worker.js
+++ Service-Worker.js
@@ -1,24 +1,28 @@
-// Basit cache destekli service worker
-const CACHE_NAME = 'sohbet-cache-v1';
+const CACHE_NAME = "sohbet-pwa-cache-v1";
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/app.js',
-  '/firebaseConfig.js',
-  '/manifest.json'
+  "./",
+  "./index.html",
+  "./app.js",
+  "./firebaseConfig.js",
+  "./manifest.json",
+  "./google-symbol.png",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./açılış .mp4",
+  "./uygulamaici.mp4"
 ];
 
-self.addEventListener('install', (event)=>{
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache)=>{
+    caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener('fetch', (event)=>{
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response)=>{
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
